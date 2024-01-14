d57dbfa1023073111b84ad6456f3095ec1a9f75a
bt-nghia <97080122+bt-nghia@users.noreply.github.com>
2024-01-14 18:02:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,15 @@ CONFIG = {
     'gpu_id': "0",
     'note': 'some_note',
     'model': 'MIDGN',
-    'dataset_name': 'iFashion_sample',
+    'dataset_name': 'iFashion',
     'task': 'tune',
     'eval_task': 'test',
 
     ## optimal hyperparameters
-    'lrs': [1e-2],
+    'lrs': [1e-3],
     'message_dropouts': [0.3],
-    'node_dropouts': [0],
-    'decays': [1e-7],
+    'node_dropouts': [0.1],
+    'decays': [4e-5],
 
     ## hard negative sample and further train
     'sample': 'simple',
@@ -25,16 +25,16 @@ CONFIG = {
     'conti_train': 'log/iFashion_sample/',
 
     ## other settings
-    'epochs': 1000,
-    'early': 50,
+    'epochs': 50,
+    'early': 20,
     'log_interval': 20,
     'test_interval': 5,
     'retry': 1,
 
     ## test path
-    'test':['log/iFashion_sample'],
-    'batch_size_train': 131072,
-    'batch_size_test': 65536,
+    'test':['log/iFashion'],
+    'batch_size_train': 2048,
+    'batch_size_test': 2048,
     'n_layers': 2,
     'corDecay': 1e-2,
     'topk_pos': 30,
